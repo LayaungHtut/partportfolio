b8fc56acbfa021771c5a14ae578b42a9a89fa6d7
LayaungHtut <layaunghtut5@gmail.com>
2025-11-05 22:07:19 +0630

    Partportfolio

--- src/lib/data/certificates.ts
+++ src/lib/data/certificates.ts
@@ -5,17 +5,17 @@ import jlpt from '$lib/assets/images/certificates/JLPT.jpg';
 export const certificates = [
     {
         certificates: cs50x,
-        title: 'cs50x',
-        description: 'fdsf',
+        title: 'CS50: Introduction to Computer Science',
+        description: 'Completed a hands-on course by Harvard University covering foundational concepts in computer science, including algorithms, data structures, and web development. (2025)',
     },
     {
         certificates: english,
-        title: 'sfds',
-        description: 'fds',
+        title: 'General English Diploma',
+        description: 'Demonstrated advanced proficiency in English through comprehensive coursework in grammar, writing, and communication skills.',
     },
     {
         certificates: jlpt,
-        title: 'afd',
-        description: 'afd',
+        title: 'JLPT N3 Certification',
+        description: 'Achieved N3 level proficiency in the Japanese-Language Proficiency Test (JLPT), demonstrating intermediate skills in reading, writing, and listening in Japanese.',
     }
 ]
